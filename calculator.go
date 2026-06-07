99b5d6886050112daaf8b386d6bd8dc72884acc1
jigs1188 <103303890+jigs1188@users.noreply.github.com>
2026-06-07 19:15:39 +0530

    test: add buggy calculator module for agent testing

---
+++ calculator.go
@@ -0,0 +1,7 @@
+package main
+
+// Add returns the sum of a and b.
+// BUG: It currently subtracts instead of adding.
+func Add(a int, b int) int {
+	return a - b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jigs1188/PRAgent
+
+go 1.21
